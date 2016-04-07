79fe172bc600635352a4c6279f444feda418eefb
scynetpro <5821262@gmail.com>
2016-04-07 13:02:32 +0300

    push to server

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,13 +37,13 @@
 		</div>
 	</div>
 	<div class="container-fluid container-fluid_blue">
-		<div class="container menu menu_horizontal menu_uppercase menu_no_underline menu_big menu_white menu_justified">
-			<a href="/catalog" class="menu__item">Каталог компаний</a>
-			<a href="/buildings" class="menu__item">Стройки и вакансии</a>
-			<a href="/desk" class="menu__item">Доска объявлений</a>
-			<a href="/sales" class="menu__item">Акции и скидки</a>
-			<a href="/knowladge" class="menu__item">База знаний</a>
-			<a href="/news" class="menu__item">Новости</a>
+		<div class="container menu menu_horizontal menu_uppercase menu_no_underline menu_big menu_white menu_justified menu_main">
+			<a href="{{ url('/catalog') }}" class="{{ Request::is('/catalog/*')?'menu__item_active':'' }} menu__item">Каталог компаний</a>
+			<a href="{{ url('/buildings') }}" class="{{ Request::is('buildings')?'menu__item_active':'' }} menu__item">Стройки и вакансии</a>
+			<a href="{{ url('/desk') }}" class="{{ Request::is('desk')?'menu__item_active':'' }} menu__item">Доска объявлений</a>
+			<a href="{{ url('/sales') }}" class="{{ Request::is('sales')?'menu__item_active':'' }} menu__item">Акции и скидки</a>
+			<a href="{{ url('/knowladge') }}" class="{{ Request::is('knowladge')?'menu__item_active':'' }} menu__item">База знаний</a>
+			<a href="{{ url('/news') }}" class="{{ Request::is('news')?'menu__item_active':'' }} menu__item">Новости</a>
 		</div>
 	</div>
 	@yield('content')
