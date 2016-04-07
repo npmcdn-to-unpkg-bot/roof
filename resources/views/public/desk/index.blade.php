beee138ed5145a5f9b25e67d6eeddfad78b2ba19
scynetpro <5821262@gmail.com>
2016-04-07 13:37:33 +0300

    push to vps

--- resources/views/public/desk/index.blade.php
+++ resources/views/public/desk/index.blade.php
@@ -9,8 +9,8 @@
 			<div class="container__col-8">
 				<div class="title">ОБЪЯВЛЕНИЯ</div>
 				<form action="" class="jus offset_vertical_20">
-					<input type="text" size="65" placeholder="КЛЮЧЕВЫЕ СЛОВА" class="input jus__item">
-					<select name="" id="" class="input_select input jus__item">
+					<input type="text" width="490" placeholder="КЛЮЧЕВЫЕ СЛОВА" class="input jus__item">
+					<select name="" width="200" class="input_select input jus__item">
 						<option value="">ВЫБИРИТЕ СТРАНУ</option>
 					</select>
 					<button class="jus__item button button_search"></button>
