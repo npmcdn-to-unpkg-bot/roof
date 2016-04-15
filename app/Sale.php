5e36c838e13ad9a6cfe0814d3945a494e378c1d7
scynetpro <5821262@gmail.com>
2016-04-15 13:34:56 +0300

    fix validation

--- app/Sale.php
+++ app/Sale.php
@@ -7,19 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     public static $rules = [
-		'title' => 'required|min:15|max:100',
+		'title' => 'required|min:5|max:100',
 		'image' => 'required',
-		'entry' => 'required|min:120|max:380',
+		'entry' => 'required|min:90|max:380',
 		'content' => 'required|min:500',
     ];
 
     public static $messages = [
 		'title.required' => 'Введите заголовок.',
-		'title.min' => 'Заголовок должен быть не меньше 15 символов',
+		'title.min' => 'Заголовок должен быть не меньше 5 символов',
 		'title.max' => 'Заголовок должен быть не больше 100 символов',
 		'image.required' => 'Загрузите картинку.',
 		'entry.required' => 'Заполните краткое содержание.',
-		'entry.min' => 'Краткое содержание должно быть не меньше 120 символов.',
+		'entry.min' => 'Краткое содержание должно быть не меньше 90 символов.',
 		'entry.max' => 'Краткое содержание должно быть не больше 380 символов.',
 		'content.required' => 'Заполните текст.',
 		'content.min' => 'Текст должен быть не меньше 300 символов.',
