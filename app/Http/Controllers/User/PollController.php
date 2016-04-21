6f47f057bcb9a3d4e7f46d2cc1a3a3c09f8c53a1
scynetpro <5821262@gmail.com>
2016-04-21 12:47:21 +0300

    poll

---
+++ app/Http/Controllers/User/PollController.php
@@ -0,0 +1,16 @@
+<?php 
+ 
+namespace App\Http\Controllers\User; 
+ 
+use Illuminate\Http\Request; 
+use Auth; 
+use App\Http\Requests; 
+use App\Http\Controllers\Controller; 
+ 
+class PollController extends Controller 
+{ 
+    public function vote (Request $request) { 
+      Auth::user()->votes()->attach($request->vote); 
+      return back(); 
+    } 
+} 
