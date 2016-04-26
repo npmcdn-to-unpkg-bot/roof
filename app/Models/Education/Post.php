234a2f250043c8f7d4988ae9596e447a7a349de0
scynetpro <5821262@gmail.com>
2016-04-26 17:56:58 +0300

    fix

--- app/Models/Education/Post.php
+++ app/Models/Education/Post.php
@@ -19,12 +19,11 @@ class Post extends Model
     public static function validator ($fields) {
     	return Validator::make($fields,
 		 	[
-				'title' => 'required|min:35|max:255',
+				'title' => 'required|max:255',
 				'entry' => 'required|min:120|max:380',
 				'content' => 'required|min:500',
 		    ],[
 				'title.required' => 'Введите заголовок записи.',
-				'title.min' => 'Заголовок должен быть не меньше 35 символов.',
 				'title.max' => 'Заголовок должен быть не больше 255 символов.',
 				'entry.required' => 'Заполните краткое содержание записи.',
 				'entry.min' => 'Краткое содержание должно быть не меньше 120 символов.',
