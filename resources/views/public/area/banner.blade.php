e09a1952ee6bdeaf185d2d1710af3e2b630e66c6
scynetpro <5821262@gmail.com>
2016-04-27 14:13:59 +0300

    fix

--- resources/views/public/area/banner.blade.php
+++ resources/views/public/area/banner.blade.php
@@ -1,6 +1,6 @@
 <?php $area = App\Area::where('name', $area)->with('banner')->first() ?>
 @if ($area&&$area->banner)
 	<a href="{{$area->banner->href}}">
-		<img src="/imagecache/full/{{$area->banner->image}}" style="display: block;" alt="">
+		<img src="/imagecache/full/{{$area->banner->image}}" style="display: block; max-width: 100%;" alt="">
 	</a>
 @endif
