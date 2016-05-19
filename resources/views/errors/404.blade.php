bdf40a5c36fe88203cee3f9eb246e8b40c8b51d2
scynetpro <5821262@gmail.com>
2016-05-19 10:32:51 +0300

    fix

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,8 +4,27 @@
 
 @section('content')
 <div class="container">
-	<div class="title"><span style="font-size: 130px;">404</span> ошибка<br>
-Страница не найдена <br>
-Давайте поищем вместе</div>
+	<div id="page-404">
+		<div class="title"><span style="font-size: 130px;">404</span> ошибка<br>
+		Страница не найдена <br>
+		Давайте поищем вместе</div>
+		<img id="image-404" src="/img/404.png" alt="">
+	</div>
+	<style>
+		#page-404 {
+			width: 546px;
+			margin: 150px auto;	
+			height: 245px;
+			padding-top: 107px;
+			font-size: 30px;
+			position: relative;
+		}
+		#image-404 {
+			position: absolute;
+			z-index: -1;
+			top: 0;
+			right: 0;
+		}
+	</style>
 </div>
 @endsection
