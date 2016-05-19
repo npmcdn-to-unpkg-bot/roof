ab063a436fc404b556b3f8ea3a5e8787f9f694b3
scynetpro <5821262@gmail.com>
2016-05-19 12:05:12 +0300

    fix

--- resources/views/general/events/show.blade.php
+++ resources/views/general/events/show.blade.php
@@ -47,6 +47,9 @@
 							Ссылка на сайт мероприятия:<br>
 							<a href="http://{{$event->website}}/">{{$event->website}}</a>
 						</div>
+						<script type="text/javascript" src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js" charset="utf-8"></script>
+						<script type="text/javascript" src="//yastatic.net/share2/share.js" charset="utf-8"></script>
+						<div class="ya-share2" data-services="facebook,gplus,twitter" data-counter=""></div>
 					</div>
 				</div>
 			</div>
