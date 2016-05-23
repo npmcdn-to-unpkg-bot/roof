3b7828129ca93c669611a53a02fa4e7d0fa16d46
scynetpro <5821262@gmail.com>
2016-05-23 12:01:38 +0300

    forum block

--- resources/views/general/forum/block.blade.php
+++ resources/views/general/forum/block.blade.php
@@ -1,33 +1,42 @@
+<?php 
+
+	$db = new mysqli(
+		"acc00954.mysql.ukraine.com.ua",
+		"acc00954_forum",
+		"h8blqs2d",
+		"acc00954_forum"
+	);
+	
+	mb_internal_encoding("UTF-8");
+	$db->set_charset("utf8");
+
+	$query_posts = $db->query('SELECT * FROM phpbb_posts ORDER BY post_time DESC LIMIT 5');
+
+	while ($post = $query_posts->fetch_assoc()){
+		$post['post_time'] = date('d.m.Y', $post['post_time']);
+		$post['post_text'] = mb_substr(
+			preg_replace(
+				'|[[\/\!]*?[^\[\]]*?]|si',
+				'',
+				strip_tags( $post['post_text']) ),
+			0,
+			150
+		);
+		$query_users = $db->query('SELECT * FROM phpbb_users WHERE user_id='.$post['poster_id']);
+		$post['user'] = $query_users->fetch_assoc();
+		$posts[] = $post;
+	}
+
+?>
+
 <div class="forum-message">
 	<div class="title">ПОСЛЕДНЕЕ НА ФОРУМЕ</div>
-	<div class="forum-message__item">
-		<img src="/img/no-name-user.jpg" alt="" class="forum-message__avatar">
-		<a href="#" class="forum-message__name">good_times</a><span class="forum-message_post-date">5 мин. назад</span>
-		<div class="forum-message__text">Как строить стропильную крышу?</div>
-		<a href="#" class="forum-message__theme">ВОПРОСЫ</a>
-	</div>
-	<div class="forum-message__item">
-		<img src="/s-img/user-1.jpg" alt="" class="forum-message__avatar">
-		<a href="#" class="forum-message__name">preZident</a><span class="forum-message_post-date">6 мин. назад</span>
-		<div class="forum-message__text">Ребята, какой вариант крыши тут можно придумать?</div>
-		<a href="#" class="forum-message__theme">Необычные архитектурные формы крыш</a>
-	</div>
-	<div class="forum-message__item">
-		<img src="/img/no-name-user.jpg" alt="" class="forum-message__avatar">
-		<a href="#" class="forum-message__name">Smitters</a><span class="forum-message_post-date">9 мин. назад</span>
-		<div class="forum-message__text">Я бы увеличил сечение конькового прогона</div>
-		<a href="#" class="forum-message__theme">СОВЕТЫ</a>
-	</div>
-	<div class="forum-message__item">
-		<img src="/s-img/user-2.jpg" alt="" class="forum-message__avatar">
-		<a href="#" class="forum-message__name">Barbos</a><span class="forum-message_post-date">11 мин. назад</span>
-		<div class="forum-message__text">Интересует диагностика кровли</div>
-		<a href="#" class="forum-message__theme">Диагностика кровли. Стоимость.</a>
-	</div>
-	<div class="forum-message__item">
-		<img src="/img/no-name-user.jpg" alt="" class="forum-message__avatar">
-		<a href="#" class="forum-message__name">malfoy</a><span class="forum-message_post-date">20 мин. назад</span>
-		<div class="forum-message__text">Здравствуйте! Коллеги, подскажите, пожалуйста, каким образом мне решить мою проблему. Фото крыши прилагаю. Заранее благодарен всем за ответы.</div>
-		<a href="#" class="forum-message__theme">Нужна помощь</a>
-	</div>
+	<?php foreach ($posts as $post): ?>
+		<div class="forum-message__item">
+			<img src="<?php if ($post['user']['user_avatar']): ?>http://forum.roofers.com.ua/download/file.php?avatar=<?php echo $post['user']['user_avatar'] ?><?php else: ?>http://roofers.com.ua/img/no-name-user.jpg<?php endif ?>" alt="" class="forum-message__avatar" width="35px">
+			<a target="blank" href="http://forum.roofers.com.ua/memberlist.php?mode=viewprofile&u=<?php echo $post['poster_id'] ?>" class="forum-message__name"><?php echo $post['user']['username'] ?></a><span class="forum-message_post-date"><?php echo $post['post_time'] ?></span>
+			<div class="forum-message__text"><?php echo $post['post_text'] ?></div>
+			<a target="blank" href="http://forum.roofers.com.ua/viewtopic.php?f=<?php echo $post['forum_id'] ?>&t=<?php echo $post['topic_id'] ?>" class="forum-message__theme"><?php echo $post['post_subject'] ?></a>
+		</div>
+	<?php endforeach ?>
 </div>
