8e9cf5d908fc0779b214cb630269f98feefd9b74
scynetpro <5821262@gmail.com>
2016-06-16 16:55:13 +0300

    fix

--- resources/views/admin/parts/table/actions.blade.php
+++ resources/views/admin/parts/table/actions.blade.php
@@ -1,5 +1,11 @@
 <form action="{{ $delete }}" method="POST">
 	{!! csrf_field() !!}
 	@if ($edit) <a href="{{ $edit }}" class="btn btn-sm btn-info" data-toggle="tooltip" data-original-title="Редактировать"><span class="glyphicon glyphicon-pencil"></span></a> @endif
-	@if ($delete) <button name="_method" value="DELETE" class="btn btn-sm btn-danger pull-right" onclick="confirm('Подтвердите удаление')" data-toggle="tooltip" data-original-title="Удалить"><span class="glyphicon glyphicon-remove"></span> </button> @endif
+	@if ($delete) <button name="_method" value="DELETE" class="btn btn-sm btn-danger pull-right" onclick="confirm_delete(event)" data-toggle="tooltip" data-original-title="Удалить"><span class="glyphicon glyphicon-remove"></span> </button> @endif
 </form>
+<script>
+	function confirm_delete(event) {
+		if (!confirm('Подтвердите удаление'))
+			event.preventDefault()
+	}
+</script>
