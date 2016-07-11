decfe9e1a47ee21a1c796e54b50de3aa3467172b
scynetpro <5821262@gmail.com>
2016-07-11 12:03:41 +0300

    fix

--- resources/views/admin/parts/form/price.blade.php
+++ resources/views/admin/parts/form/price.blade.php
@@ -1,12 +1,13 @@
 <div class="form-group {{$errors->first($name)?'has-error':''}}">
 	<label class="control-label" for="{{$name}}">{{$label}}</label>
 	<div class="form-inline">
-		<input class="form-control" type="number" value="{{ trim(str_replace(['грн.','руб.','$'],'',$value)) }}" onchange="concat_price()" id="price_{{$name}}">
+		<input class="form-control" type="number" value="{{ trim(str_replace(['грн.','руб.','$','€'],'',$value)) }}" onchange="concat_price()" id="price_{{$name}}">
 		<select class="form-control" onchange="concat_price()" id="currency_{{$name}}">
 			<option value=""></option>
 			<option {{strpos($value,'грн.')?'selected':''}}>грн.</option>
 			<option {{strpos($value,'руб.')?'selected':''}}>руб.</option>
 			<option {{strpos($value,'$')?'selected':''}}>$</option>
+			<option {{strpos($value,'€')?'selected':''}}>€</option>
 		</select>
 	</div>
 	<input type="hidden" value="{{ $value }}" name="{{$name}}" id="{{$name}}">
