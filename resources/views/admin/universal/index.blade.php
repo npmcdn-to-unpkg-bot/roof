451e1a9ad31e3c05f9b6c1dc27e6a3983ad4ec69
scynetpro <5821262@gmail.com>
2016-07-13 18:50:02 +0300

    fix

--- resources/views/admin/universal/index.blade.php
+++ resources/views/admin/universal/index.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <section class="content">
     	<h1>{{$title}} @if (isset($add)) <a href="{{$add}}" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Добавить</a> @endif </h1>
+		@if(isset($message))
+			{!!$message!!}
+		@endif
     	@include('admin.parts.table.index')
     </section>
 @endsection
