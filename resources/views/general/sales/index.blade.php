30e78644ae7fb9b5979a61d9c8872ec64c9bd667
scynetpro <5821262@gmail.com>
2016-08-01 13:18:44 +0300

    fix

--- resources/views/general/sales/index.blade.php
+++ resources/views/general/sales/index.blade.php
@@ -13,7 +13,7 @@
 				<div class="market-news offset_bottom_30 offset-sm_bottom_30">
 					@if ($sale->image) <a href="{{route('sales.show', $sale)}}"><img src="/fit/240/200/{{$sale->image}}" alt="" class="market-news__image"></a> @endif
 					<a href="{{route('sales.show', $sale)}}" class="market-news__title">{{$sale->title}}</a>
-					<div class="market-news__text">{{$sale->entry}}</div>
+					<div class="market-news__text @if($sale->company&&$sale->company->level == 3) text_bold @endif">{{$sale->entry}}</div>
 				</div>
 				@endforeach
 
