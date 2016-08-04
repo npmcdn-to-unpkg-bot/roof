dc66a67bf9458b8552eac3dda67cae3ea294f9e7
scynetpro <5821262@gmail.com>
2016-08-04 11:18:55 +0300

    fix

--- resources/views/general/wantroof/mail.blade.php
+++ resources/views/general/wantroof/mail.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div style="color: #0a3955;text-transform: uppercase;font-weight: bold;text-align: center;">Отправление формы "Хочу кровлю"</div>
-<div>Тема заявки: <strong>{{$theme}}</strong></div>
-<div>Имя: <strong>{{$name}}</strong></div>
-<div>Телефон: <strong>{{$phone}}</strong></div>
-<div>Email: <strong><a href="mailto:{{$email}}">{{$email}}</a></strong></div>
-<div>Комментарий: <strong>{{$comment}}</strong></div>
+<div style="font-size: 18px;">Тема заявки: <strong>{{$theme}}</strong></div>
+<div style="font-size: 18px;">Имя: <strong>{{$name}}</strong></div>
+<div style="font-size: 18px;">Телефон: <strong>{{$phone}}</strong></div>
+<div style="font-size: 18px;">Email: <strong><a href="mailto:{{$email}}">{{$email}}</a></strong></div>
+<div style="font-size: 18px;">Комментарий: <strong>{{$comment}}</strong></div>
 @endsection
